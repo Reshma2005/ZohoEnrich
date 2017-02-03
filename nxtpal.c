57e023d412a61d81766058ed486ce41ef76e5cce
Reshma MN <reshmanandagopal@gmail.com>
2017-02-03 23:33:35 +0530

    Update nxtpal.c

--- nxtpal.c
+++ nxtpal.c
@@ -1,20 +1,26 @@
 #include<stdio.h>
+
 int main()
 {
 	int num,a[10],i,j,max=0,m,l,r,palindrome;
+	
 	printf("Enter the number:\n");
 	scanf("%d",&num);
+	
 	do
 	{
 		a[max]=num%10;
 		num=num/10;
 		max++;
 	}while(num!=0);
+	
 	m=max/2;
+	
 	if(max%2!=0)
 	{
 		l=1;
 		r=1;
+		
 		if(a[m-1]>=a[m+1])
 		{
 			if(a[m]<9)
@@ -24,6 +30,7 @@ int main()
 			else
 			{
 				a[m]=0;
+				
 				for(i=m+1;i<max;i++)
 				{
 					if(a[i]==9)
@@ -39,6 +46,7 @@ int main()
 			}
 		}
 	}
+	
 	else
 	{
 		l=1;
@@ -59,20 +67,24 @@ int main()
 				}
 		}
 	}
+	
 	while(l<=m)
 	{
 		a[m-l]=a[m+r];
 		l++;
 		r++;
 	}
+	
 	for(i=max-1;i>=0;i--)
 	{
 		palindrome=palindrome*10+a[i];
 	}
+	
 	if(palindrome!=0)
 	{
 		printf("\nNext Palindrome: %d\n",palindrome);
 	}
+	
 	else
 	{
 		printf("\nNext Palindrome: 1");
@@ -82,6 +94,7 @@ int main()
 		}
 		printf("1\n");
 	}
+	
 	return 0;
 }
 
