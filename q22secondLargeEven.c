88847a98d2bd53f43bf191c0dfb831848f841d18
Reshma MN <reshmanandagopal@gmail.com>
2017-03-20 20:54:44 +0530

    Update q22secondLargeEven.c

--- q22secondLargeEven.c
+++ q22secondLargeEven.c
@@ -61,7 +61,9 @@ void findSecondMaxEven(int *a,int n)
 int main()
 {
 	int n,a[50],i;
+	printf("Enter the number of elements:");
 	scanf("%d",&n);
+	printf("\nEnter the elements:\n);
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",(a+i));
